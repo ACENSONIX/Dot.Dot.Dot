d40dc3ed19e1cefdcda6be2e890a93ecf70f33b6
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2023-04-09 08:56:04 +0530

    captcha done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,14 +6,26 @@ import Header from "./Header";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "../actions/owner/owner";
+import ReactCaptcha from "modern-react-captcha";
+import { useState } from "react";
+import CaptchaTest from "./Captcha";
 
 const Form = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const handleSuccess = () => {
+    setIsDisabled(false);
+  };
+  const handleFailure = () => {
+    setIsDisabled(true);
+  };
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleFormSubmit = values => {
+  const handleFormSubmit = (values) => {
     console.log(values);
     try {
       const data = {
@@ -26,52 +38,81 @@ const Form = () => {
     }
   };
 
+  const handleDisabled = () => {
+    setIsDisabled(!isDisabled);
+  };
+
   return (
-    <Box display='grid' sx={{ placeItems: "center", height: "100%" }}>
+    <Box display="grid" sx={{ placeItems: "center", height: "100%" }}>
       <Formik
         onSubmit={handleFormSubmit}
         initialValues={initialValues}
-        validationSchema={checkoutSchema}>
-        {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
+        validationSchema={checkoutSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
           <form onSubmit={handleSubmit}>
-            <Header title='LOGIN' subtitle='Login into your profile' />
+            <Header title="LOGIN" subtitle="Login into your profile" />
             <Box
-              display='grid'
-              gap='30px'
-              gridTemplateColumns='repeat(4, minmax(0, 1fr))'
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
               sx={{
                 "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
                 minWidth: "40vw",
-              }}>
+              }}
+            >
               <TextField
                 fullWidth
-                variant='filled'
-                type='text'
-                label='Email'
+                variant="filled"
+                type="text"
+                label="Email"
                 onBlur={handleBlur}
                 onChange={handleChange}
                 value={values.email}
-                name='email'
+                name="email"
                 error={!!touched.email && !!errors.email}
                 helperText={touched.email && errors.email}
                 sx={{ gridColumn: "span 4" }}
               />
               <TextField
                 fullWidth
-                variant='filled'
-                type='password'
-                label='Password'
+                variant="filled"
+                type="password"
+                label="Password"
                 onBlur={handleBlur}
                 onChange={handleChange}
                 value={values.password}
-                name='password'
+                name="password"
                 error={!!touched.password && !!errors.password}
                 helperText={touched.password && errors.password}
                 sx={{ gridColumn: "span 4" }}
               />
             </Box>
-            <Box display='grid' mt='20px'>
-              <Button type='submit' color='secondary' variant='contained'>
+            <Box sx={{display:'flex', flexDirection:'column', alignItems:'center', mt:3}}>
+              {/* <ReactCaptcha
+                charset="uln"
+                color="random"
+                bgColor="yellow"
+                reload
+                handleSuccess={handleSuccess}
+                handleFailure={handleFailure}
+              /> */}
+              <CaptchaTest handleDisabled={handleDisabled} />
+            </Box>
+            <Box display="grid" mt="20px">
+              <Button
+                type="submit"
+                color="secondary"
+                variant="contained"
+                // disabled={isDisabled}
+              >
                 Login
               </Button>
             </Box>
